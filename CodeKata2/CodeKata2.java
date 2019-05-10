e12c06d95d9a1d4b3a7c3ca9f18aaafdc0fe8254
VaishnaviRamabhadran <vaishu2395@gmail.com>
2019-05-11 00:37:46 +0530

    Created CodeKata2.java

---
+++ CodeKata2/CodeKata2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+class CodeKata2{
+public static void main(String[] args)
+{
+int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+System.out.print((n%2==0)?"Even":(n%2!=0)?"Odd":"Invalid");
+}
+}

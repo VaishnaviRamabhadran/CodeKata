a52002cadae299c43992a153b22a675bfa0bd695
VaishnaviRamabhadran <vaishu2395@gmail.com>
2019-05-11 18:13:35 +0530

    CodKata20

---
+++ CodeKata20/CodeKata20.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+class CodeKata20{
+public static void main(String[] args)
+{
+int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+ for(int i=1;i<=5;i++)
+ {
+	System.out.print((n*i)+" ");
+ }
+}
+}

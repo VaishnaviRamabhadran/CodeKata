0b275a395288e1354991f2ca1e1efda024b552ca
VaishnaviRamabhadran <vaishu2395@gmail.com>
2019-05-11 13:20:51 +0530

    CodeKata7

---
+++ CodeKata7/CodeKata7.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+class CodeKata7{
+public static void main(String[] args)
+{
+int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+for(int i=0;i<n;i++)
+System.out.println("Hello");
+}
+}

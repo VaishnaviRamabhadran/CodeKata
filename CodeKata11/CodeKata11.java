a1221e6539ae33b11e6a93202b1e6102448bb2be
VaishnaviRamabhadran <vaishu2395@gmail.com>
2019-05-11 15:06:00 +0530

    CodeKata11

---
+++ CodeKata11/CodeKata11.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+class CodeKata11{
+public static void main(String[] args)
+{
+int n,k;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+k=sc.nextInt();
+System.out.println(Math.pow(n,k));
+}
+}

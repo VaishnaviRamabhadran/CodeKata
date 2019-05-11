72bfb20e77911be16657b19eba37cd6149fec4a9
VaishnaviRamabhadran <vaishu2395@gmail.com>
2019-05-11 18:01:02 +0530

    CodeKata19

---
+++ CodeKata18/CodeKata18.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+class CodeKata18{
+public static void main(String[] args)
+{
+int a,b,n,sum;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+ for(int i=a+1;i<b;i++)
+ {
+  n=i;
+  sum=0;
+  while(n!=0)
+  {
+  sum+=Math.pow(n%10,3);
+  n=n/10;
+  }
+  if(i==sum)
+  {
+   System.out.print(sum+ " ");
+  }
+ }
+}
+}
